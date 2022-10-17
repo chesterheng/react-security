3a110fec2b9802aaa603ef3bcef10c252b2de0e4
Chester Heng <chester.heng@gmail.com>
2022-10-17 22:23:20 +0800

    4.Handling Client-Side Routing

--- orbit-app/src/App.js
+++ orbit-app/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+  Switch,
+  Redirect
 } from 'react-router-dom';
 import './App.css';
 import AppShell from './AppShell';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, AuthContext } from './context/AuthContext';
 import { FetchProvider } from './context/FetchContext';
 import Account from './pages/Account';
 import Dashboard from './pages/Dashboard';
@@ -17,6 +18,36 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Signup from './pages/Signup';
 import Users from './pages/Users';
+
+const AuthenticatedRoute = ({ children, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  return (
+    <Route { ...rest } render={() => 
+      authContext.isAuthenticated() ? (
+        <AppShell>
+          {children}
+        </AppShell>
+      ) : (
+        <Redirect to='/' />
+      )
+    }/>
+  )
+}
+
+const AdminRoute = ({ children, ...rest }) => {
+  const authContext = useContext(AuthContext);
+  return (
+    <Route { ...rest } render={() => 
+      authContext.isAuthenticated()  && authContext.isAdmin() ? (
+        <AppShell>
+          {children}
+        </AppShell>
+      ) : (
+        <Redirect to='/' />
+      )
+    }/>
+  )
+}
 
 const AppRoutes = () => {
   return (
@@ -30,31 +61,21 @@ const AppRoutes = () => {
       <Route exact path="/">
         <Home />
       </Route>
-      <Route path="/dashboard">
-        <AppShell>
-          <Dashboard />
-        </AppShell>
-      </Route>
-      <Route path="/inventory">
-        <AppShell>
-          <Inventory />
-        </AppShell>
-      </Route>
-      <Route path="/account">
-        <AppShell>
-          <Account />
-        </AppShell>
-      </Route>
-      <Route path="/settings">
-        <AppShell>
-          <Settings />
-        </AppShell>
-      </Route>
-      <Route path="/users">
-        <AppShell>
-          <Users />
-        </AppShell>
-      </Route>
+      <AuthenticatedRoute path='/dashboard'>
+        <Dashboard />
+      </AuthenticatedRoute>
+      <AdminRoute path="/inventory">
+        <Inventory />
+      </AdminRoute>
+      <AuthenticatedRoute path="/account">
+        <Account />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AdminRoute path="/users">
+        <Users />
+      </AdminRoute>
       <Route path="*">
         <FourOFour />
       </Route>
